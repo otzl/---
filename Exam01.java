2f5ac9c19197dde8f554c9f11f27c44916f5bed7
우지 <95010744+otzl@users.noreply.github.com>
2021-12-05 16:40:58 +0900

    Add files via upload

---
+++ Exam01.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+
+		String str = "ZAbcdVefEg";
+		reverseStr(str);
+		System.out.println(reverseStr(str));
+
+	}
+
+	private static String reverseStr(String str) {
+		
+		String[] a = str.split("");
+		Arrays.sort(a);
+		return String.join("", a);
+	}
+
+}
