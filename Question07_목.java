713d78838e5df81ece78f2bc035a7db10f702fee
권우진 <95010744+otzl@users.noreply.github.com>
2022-03-23 12:47:22 +0900

    Update Question07_목.java

--- Question07_목.java
+++ Question07_목.java
@@ -6,7 +6,7 @@ public class Question07_목 {
  
 	public static void main(String[] args) { 
 
-		// 10진수 정수를 입력받아 2진수로 변환해서 출력하시오.
+		// 10진수 정수를 입력받아 2진수로 변환해서 출력하시오. 
  
 		Scanner sc = new Scanner(System.in);
 
